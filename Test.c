a0a1e4d0e8a5b501bc9f11626ea97063043ff872
kousiga-03 <askousiga@gmail.com>
2025-07-26 17:25:56 +0530

    Update Test.c

--- Test.c
+++ Test.c
@@ -3,4 +3,8 @@ void main(){
   int a=0;
   int b=0;
   printf("print a and b");
+  //from main branch 
+  printf("The value of a is %d",a);
+  //from dev branch 
+  printf("The value of b is %d",b);
 }
